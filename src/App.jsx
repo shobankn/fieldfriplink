b9136d50ac1c1a18540d6edc0273e5c3d2630f6b
harisaziz850 <harisaziz850@gmail.com>
2025-07-31 00:13:39 -0700

    Router

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,19 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css'
+import Home from '../src/landgingpage/Home/Home'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <>
-     
-      <h1  className='text-[100px]'> React</h1>
-    
-    </>
+   <Router>
+    <Routes>
+      <Route path='/' element={<Home/>}/>
+    </Routes>
+   </Router>
   )
 }
 
---
+++ src/landgingpage/Home/Home.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <div>
+            <div class="flex bg-[#d02323]">
+  <div class="w-1/2  bg-[#d02323]">Column 1</div>
+  <div class="w-1/2">Column 2</div>
+</div>
+
+    </div>
+  )
+}
+
+export default Home
